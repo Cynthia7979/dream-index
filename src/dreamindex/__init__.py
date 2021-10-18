f4a09e4c1913203d0d7236ca03ee1536dde08109
Cynthia7979 <CynthiaWang7979@hotmail.com>
2021-10-18 15:45:55 +0800

    Specialized __init__.py to creating app instance and setup package only

--- src/dreamindex/__init__.py
+++ src/dreamindex/__init__.py
@@ -1,29 +1,10 @@
+"""
+Defines the `app` instance
+"""
+
 from flask import Flask
-from flask import render_template
 
-app = Flask(__name__)
+app = Flask(__name__)  # Flask Documentation: Import arbitrary modules *after* creating the app instance
 
-
-@app.route('/')
-def home_page():
-    return render_template('home_page.html')
-
-
-@app.route('/rand/dream')
-def random_dream():
-    return '<p>Hello, World!</p>'
-
-
-@app.route('/rand/fanart')
-def random_fanart():
-    return '<p>Hello, World!</p>'
-
-
-@app.route('/new/dream')
-def create_dream():
-    return '<p>Hello, World!</p>'
-
-
-@app.route('/new/fanart')
-def create_fanart(dream_url):
-    return '<p>Hello, World!</p>'
+import dreamindex.views
+import dreamindex.instances
