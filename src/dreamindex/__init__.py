2c372b4a3e8360b87a34c714d4d2b41fd74bbf23
Cynthia7979 <CynthiaWang7979@hotmail.com>
2022-02-20 19:54:46 +0800

    Added new files to __init__.py

--- src/dreamindex/__init__.py
+++ src/dreamindex/__init__.py
@@ -4,12 +4,15 @@ Defines the `app` instance
 
 from flask import Flask
 import os
-import dreamindex.database_handling
 
 app = Flask(__name__)  # Flask Documentation: Import arbitrary modules *after* creating the app instance
+
+import dreamindex.database_handling
 db = dreamindex.database_handling.Database(database_path=os.path.dirname(__file__)+"/databases/test_database.db")
 
 import dreamindex.views
 import dreamindex.instances
 import dreamindex.logging
+import dreamindex.cookies
+import dreamindex.forms
 # setup_database is not imported because it is not part of the package.
