a1caf02f58eef7fedfdac8e51f005d33f5ea3d71
Cynthia7979 <CynthiaWang7979@hotmail.com>
2022-02-20 19:34:48 +0800

    Fixed a likely typo

--- src/dreamindex/__init__.py
+++ src/dreamindex/__init__.py
@@ -7,7 +7,7 @@ import os
 import dreamindex.database_handling
 
 app = Flask(__name__)  # Flask Documentation: Import arbitrary modules *after* creating the app instance
-db = database_handling.Database(database_path=os.path.dirname(__file__)+"/databases/test_database.db")
+db = dreamindex.database_handling.Database(database_path=os.path.dirname(__file__)+"/databases/test_database.db")
 
 import dreamindex.views
 import dreamindex.instances
